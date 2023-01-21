69312f15ea9dc988376db03f57409b1b8baf5871
riku-erikmaki <riku-erik.maki@aalto.fi>
2023-01-21 13:10:59 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,39 @@
+import { serve } from "https://deno.land/std@0.171.0/http/server.ts";
+import { configure, renderFile } from "https://deno.land/x/eta@v1.12.3/mod.ts";
+
+configure({
+  views: `${Deno.cwd()}/views/`,
+});
+
+const responseDetails = {
+  headers: { "Content-Type": "text/html;charset=UTF-8" },
+};
+
+const data = {
+  names: [],
+};
+
+const redirectTo = (path) => {
+  return new Response(`Redirecting to ${path}.`, {
+    status: 303,
+    headers: {
+      "Location": path,
+    },
+  });
+};
+
+const addName = async (request) => {
+  const formData = await request.formData();
+  data.names.push(formData.get("name"));
+};
+
+const handleRequest = async (request) => {
+  if (request.method === "POST") {
+    await addName(request);
+    return redirectTo(request.url);
+  }
+
+  return new Response(await renderFile("index.eta", data), responseDetails);
+};
+
+serve(handleRequest, { port: 7777 });
